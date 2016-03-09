effa98656567dac5e0de27a51b9371a267112673
Frichetten <frichetten@gmail.com>
2016-03-08 23:31:11 -0600

    Added input new blood pressure feature. Next step, fix reading from csv

--- bloodPressue.py
+++ bloodPressue.py
@@ -4,14 +4,16 @@ import matplotlib
 import datetime
 import time
 
-def displayPlot(data, systolic):
+
+def displayPlot(data, systolic, diastolic):
     listDates = []
     for time in data:
         listDates.append(datetime.datetime.fromtimestamp(float(time)))
 
     plt.gca().xaxis.set_major_formatter(matplotlib.dates.DateFormatter('%m/%d/%y'))
     plt.gca().xaxis.set_major_locator(matplotlib.dates.DayLocator())
-    plt.plot(listDates, systolic)
+    plt.plot(listDates, systolic, 'r')
+    plt.plot(listDates, diastolic, 'g')
     plt.show()
 
 def fileIO():
@@ -33,19 +35,44 @@ def main():
         print "Welcome to Blood Pressure Monitor."
         print "Please Select an Option. '/' to Exit"
         print '(1) Input New Entry'
+        print '(2) Display Systolic and Diastolic Data'
         inp = raw_input("in<")
 
+        if inp == '1':
+            #Input New Entry
+            file = open('data.csv', 'a')
+            date = time.time()
+            morn = raw_input("Morning or Night (m/n): ")
+            read1 = raw_input("First Reading(Ex: 100/50): ")
+            read2 = raw_input("Second Reading(Ex: 100/50): ")
+            read3 = raw_input("Third Reading(Ex: 100/50): ")
+            #Calculate Average Blood Pressure
+            A = read1[:read1.index('/')]
+            B = read2[:read2.index('/')]
+            C = read3[:read3.index('/')]
+            avgSystolic = (int(A)+int(B)+int(C))/3
+            a = read1[read1.index('/')+1:]
+            b = read2[read2.index('/')+1:]
+            c = read3[read3.index('/')+1:]
+            avgDiastolic = (int(a)+int(b)+int(c))/3
+            avgBP = str(avgSystolic) + '/' + str(avgDiastolic)
+            #Option to Enter Weight
+            weight = raw_input("Enter Weight? ('n' for none): ")
+            
+            file.write(str(date)+','+morn+','+read1+
+                    ','+read2+','+read3+
+                    ','+avgBP+','+weight);
+            file.close()
+        elif inp == '2':
+            #Display Systolic and Diastolic Data
+            reader = csv.reader(fileIO())
+            data = []
+            for row in reader:
+                data.append(row[0])
 
-    #reader = csv.reader(fileIO())
-
-    #data = []
-    #for row in reader:
-     #   data.append(row[0])
-
-    #systolic = [118, 129, 113, 136, 125]
-
-    #displayPlot(data[1:], systolic)
-
+            systolic = [118, 129, 113, 136, 125, 136]
+            diastolic = [75, 77, 69, 84, 81, 80]
+            displayPlot(data[1:], systolic, diastolic)
 
 
 if __name__ == '__main__':
